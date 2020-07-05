ae9f4d5b52d4ba639107eddf621c637581296d48
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-05 16:24:37 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,11 +4,9 @@ describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
-  end
-end 
+  end 
 
 
- 
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
